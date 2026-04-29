238b47a59624a037d07a8e362519ede36d7b5b86
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-04-29 14:43:08 +0530

    Add: count-the-number-of-consistent-strings - submission-0

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+        for (String word : words) {
+            boolean isValid = true;
+            for (char c : word.toCharArray()) {
+                if (allowed.indexOf(c) == -1) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
