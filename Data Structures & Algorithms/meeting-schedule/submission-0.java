988c94e0390e4007b317bdddda5c90b72e75717a
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-07-08 16:00:15 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, (a, b) -> a.start - b.start);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals.get(i).start < intervals.get(i - 1).end) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
