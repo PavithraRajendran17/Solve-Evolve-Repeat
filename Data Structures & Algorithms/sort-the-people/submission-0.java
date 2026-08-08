9e954207966aa204699d79503cda466fab77abbb
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-08-08 20:15:30 +0530

    Add: sort-the-people - submission-0

---
+++ Data Structures & Algorithms/sort-the-people/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (heights[i] < heights[j]) {
+                    int tempHeight = heights[i];
+                    heights[i] = heights[j];
+                    heights[j] = tempHeight;
+                    String tempName = names[i];
+                    names[i] = names[j];
+                    names[j] = tempName;
+                }
+            }
+        }
+        return names;
+    }
+}
