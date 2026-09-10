57e8acf2e08df6d33f0d505cc1e74cc43aa1a940
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-09-10 14:30:10 +0530

    Add: concatenated-words - submission-0

---
+++ Data Structures & Algorithms/concatenated-words/submission-0.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            set.add(word);
+        }
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+
+            set.remove(word);
+
+            int n = word.length();
+            boolean[] dp = new boolean[n + 1];
+            dp[0] = true;
+
+            for (int i = 1; i <= n; i++) {
+
+                for (int j = 0; j < i; j++) {
+
+                    if (dp[j] && set.contains(word.substring(j, i))) {
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            if (dp[n]) {
+                result.add(word);
+            }
+
+            set.add(word);
+        }
+
+        return result;
+        
+    }
+}
