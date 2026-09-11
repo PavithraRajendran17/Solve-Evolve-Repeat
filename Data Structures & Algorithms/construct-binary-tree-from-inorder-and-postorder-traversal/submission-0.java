f0c537881ffeac37b6b69ba09d36db0190c8e57b
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-09-11 20:13:23 +0530

    Add: construct-binary-tree-from-inorder-and-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/construct-binary-tree-from-inorder-and-postorder-traversal/submission-0.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return build(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1);
+    }
+
+    public TreeNode build(int[] inorder, int[] postorder, int inStart, int inEnd, int postStart, int postEnd) {
+        if (inStart > inEnd || postStart > postEnd) {
+            return null;
+        }
+
+        int rootValue = postorder[postEnd];
+        TreeNode root = new TreeNode(rootValue);
+
+        int index = inStart;
+
+        while (inorder[index] != rootValue) {
+            index++;
+        }
+
+        int leftSize = index - inStart;
+
+        root.left = build(inorder, postorder, inStart, index - 1,
+                postStart, postStart + leftSize - 1);
+
+        root.right = build(inorder, postorder, index + 1, inEnd,
+                postStart + leftSize, postEnd - 1);
+
+        return root;
+        
+    }
+}
