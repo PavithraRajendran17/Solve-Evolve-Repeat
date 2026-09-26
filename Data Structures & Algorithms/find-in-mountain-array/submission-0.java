b7384fb7961e3f7a96a44496e261e7b9ebbe8506
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-09-26 16:36:22 +0530

    Add: find-in-mountain-array - submission-0

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-0.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n = mountainArr.length();
+
+        int low = 0, high = n - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (mountainArr.get(mid) < mountainArr.get(mid + 1)) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        int peak = low;
+
+        low = 0;
+        high = peak;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int value = mountainArr.get(mid);
+
+            if (value == target) {
+                return mid;
+            } else if (value < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        low = peak + 1;
+        high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int value = mountainArr.get(mid);
+
+            if (value == target) {
+                return mid;
+            } else if (value > target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
